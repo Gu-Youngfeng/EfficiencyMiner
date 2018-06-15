50b9589ce7c8fe2e52fc688233929809c7bcdcd3
Youngfeng <yongfeng_gu@163.com>
2018-06-16 00:26:14 +0800

    Path in Test Case
    Modify the absolute path to relative path in CLSAnalyzerTest to make sure the pass of test case.

--- src/test/java/cn/edu/whu/cstar/timer/CLSAnalyzerTest.java
+++ src/test/java/cn/edu/whu/cstar/timer/CLSAnalyzerTest.java
@@ -8,132 +8,132 @@ public class CLSAnalyzerTest {
 
 	@Test
 	public void testgetInherited_0(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
 		int isherited = alser.getInherited();
 		Assert.assertEquals(0, isherited);
 	}
 	
 	@Test
 	public void testgetInherited_1(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
 		int isherited = alser.getInherited();
 		Assert.assertEquals(1, isherited);
 	}
 	
 	@Test
 	public void testgetInherited_2(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
 		int isherited = alser.getInherited();
 		Assert.assertEquals(1, isherited);
 	}
 	
 	@Test
 	public void testgetFields_0(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
 		int fields = alser.getFields();
 		Assert.assertEquals(1, fields);
 	} 
 	
 	@Test
 	public void testgetFields_1(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
 		int fields = alser.getFields();
 		Assert.assertEquals(1, fields);
 	} 
 	
 	@Test
 	public void testgetFields_2(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
 		int fields = alser.getFields();
 		Assert.assertEquals(1, fields);
 	}
 	
 	@Test
 	public void testgetLocalVariables_0(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
 		int fields = alser.getLocalVariables();
 		Assert.assertEquals(0, fields);
 	} 
 	
 	@Test
 	public void testgetLocalVariables_1(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
 		int fields = alser.getLocalVariables();
 		Assert.assertEquals(4, fields);
 	} 
 	
 	@Test
 	public void testgetLocalVariables_2(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
 		int fields = alser.getLocalVariables();
 		Assert.assertEquals(2, fields);
 	}
 	
 	@Test
 	public void testgetMethods_0(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
 		int fields = alser.getMethods();
 		Assert.assertEquals(11, fields);
 	} 
 	
 	@Test
 	public void testgetMethods_1(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
 		int fields = alser.getMethods();
 		Assert.assertEquals(54, fields);
 	} 
 	
 	@Test
 	public void testgetMethods_2(){
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
 		int fields = alser.getMethods();
 		Assert.assertEquals(2, fields);
 	}
 	
 	@Test
 	public void testgetComments_0() throws Exception{
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
-		String fullpath = "E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/net/BCodec.java";
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
+		String fullpath = "src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/net/BCodec.java";
 		int fields = alser.getComments(fullpath);
 		Assert.assertEquals(130, fields);
 	} 
 	
 	@Test
 	public void testgetComments_1() throws Exception{
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
-		String fullpath = "E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/B64.java";
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
+		String fullpath = "src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/B64.java";
 		int fields = alser.getComments(fullpath);
 		Assert.assertEquals(53, fields);
 	} 
 	
 	@Test
 	public void testgetComments_2() throws Exception{
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
-		String fullpath = "E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/DigestUtils.java";
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
+		String fullpath = "src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/DigestUtils.java";
 		int fields = alser.getComments(fullpath);
 		Assert.assertEquals(567, fields);
 	}
 	
 	@Test
 	public void testgetImports_0() throws Exception{
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
-		String fullpath = "E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/net/BCodec.java";
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.net.BCodec");
+		String fullpath = "src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/net/BCodec.java";
 		int fields = alser.getImports(fullpath);
 		Assert.assertEquals(8, fields);
 	} 
 	
 	@Test
 	public void testgetImports_1() throws Exception{
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
-		String fullpath = "E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/B64.java";
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.B64");
+		String fullpath = "src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/B64.java";
 		int fields = alser.getImports(fullpath);
 		Assert.assertEquals(1, fields);
 	} 
 	
 	@Test
 	public void testgetImports_2() throws Exception{
-		alser = new CLSAnalyzer("E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
-		String fullpath = "E:/workspaceee/EffciencyMinner/src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/DigestUtils.java";
+		alser = new CLSAnalyzer("src/main/resources/projs/Codec_parent/", "org.apache.commons.codec.digest.DigestUtils");
+		String fullpath = "src/main/resources/projs/Codec_parent/src/main/java/org/apache/commons/codec/digest/DigestUtils.java";
 		int fields = alser.getImports(fullpath);
 		Assert.assertEquals(6, fields);
 	}
