a583a59ce660c6d4b8774a6048de3cf64e11aa74
yongfeng <yongfeng_gu@163.com>
2018-06-20 19:16:19 +0800

    finish timer package

---
+++ src/main/java/cn/edu/whu/cstar/timer/Timer.java
@@ -0,0 +1,61 @@
+package cn.edu.whu.cstar.timer;
+
+public class Timer {
+	
+	public static void main(String[] args){
+		System.out.println("-------  Time Counter  -------");
+		long startTime = 0l;
+		long endTime = 0l;		
+		startTime = System.currentTimeMillis();
+		
+		// Analysis Process
+		analyzing(7);
+		
+		endTime = System.currentTimeMillis();
+		System.out.println("[Time Duration]: " + (endTime-startTime));
+	}
+	
+	public static void analyzing(int projId){
+		
+		String path = "";
+		String proj = "";
+		
+		switch(projId){
+		case 1:
+			path = "src/main/resources/crashrep/codec_mutants.txt";
+			proj = "src/main/resources/projs/Codec_parent/";
+			break;
+		case 2:
+			path = "src/main/resources/crashrep/collection_mutants.txt";
+			proj = "src/main/resources/projs/Collection_4.1_parent/";
+			break;
+		case 3:
+			path = "src/main/resources/crashrep/io_mutants.txt";
+			proj = "src/main/resources/projs/Commons-io-2.5_parent/";
+			break;
+		case 4:
+			path = "src/main/resources/crashrep/jsoup_mutants.txt";
+			proj = "src/main/resources/projs/jsoup_parent/";
+			break;
+		case 5:
+			path = "src/main/resources/crashrep/jsqlpraser_mutants.txt";
+			proj = "src/main/resources/projs/JSQL_parent/";
+			break;
+		case 6:
+			path = "src/main/resources/crashrep/mango_mutants.txt";
+			proj = "src/main/resources/projs/mango_parent/";
+			break;
+		case 7:
+			path = "src/main/resources/crashrep/ormlite_mutants.txt"; //need complete dependencies libs
+			proj = "src/main/resources/projs/ormlite_parent/";
+			break;
+		default:
+			System.out.println("[ERROR]: No such project id <" + projId + ">");
+			break;
+		}
+		
+		// analysis the program and get features
+		RepsUtilier.getFeatures(path, proj);
+	}
+
+}
