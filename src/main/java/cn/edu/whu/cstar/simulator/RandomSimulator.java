927bb116a2af9674077fbd1dbfccae06db689ade
yongfeng <yongfeng_gu@163.com>
2018-06-22 14:32:55 +0800

    complete the function in RandomSimulator

--- src/main/java/cn/edu/whu/cstar/simulator/RandomSimulator.java
+++ src/main/java/cn/edu/whu/cstar/simulator/RandomSimulator.java
@@ -11,9 +11,24 @@ public class RandomSimulator {
 
 	public static void main(String[] args) {
 		
+		List<Integer> lsFinalCrashes = getDataset("src/main/resources/crashrep/codec_mutants.txt", 500, 1);
+		showList(lsFinalCrashes);
+	}
+	
+	/**<p>using projects' stack trace file to randomly select 500 crash. 
+	 * We just simulate the randomization and selection process.</p>
+	 * <p>1. <b>Randomization:</b> swapping the j-th crash and the i-th crash, where i &in; [0, j].</p>
+	 * <p>2. <b>Selection:</b> selecting inTrace and outTrace crash according to their ratio, 
+	 * and put them into lsFinalCrashes.</p>
+	 * @param proj project's stack trace file
+	 * @param seed random seed
+	 * @return lsFinalCrashes the list of index
+	 * */
+	public static List<Integer> getDataset(String proj, int selNum, int seed){
+		
 		List<CrashNode> lsCrash = new ArrayList<CrashNode>();
 		try {
-			lsCrash = RepsUtilier.getSingleCrash("src/main/resources/crashrep/codec_mutants.txt");
+			lsCrash = RepsUtilier.getSingleCrash(proj);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -22,7 +37,13 @@ public class RandomSimulator {
 		List<Integer> lsInTrace = new ArrayList<Integer>();
 		List<Integer> lsOutTrace = new ArrayList<Integer>();
 		
-		for(int i=0; i<lsCrash.size(); i++){
+		int lenCrash = lsCrash.size();
+		if(selNum > lenCrash){
+			System.out.printf("[Error]: We cannot select %d from %d in %s.\n", selNum, lenCrash, proj);
+			return null;
+		}
+		
+		for(int i=0; i<lenCrash; i++){
 			if(lsCrash.get(i).InTrace >= 0){
 				lsInTrace.add(i);
 			}else{
@@ -31,14 +52,50 @@ public class RandomSimulator {
 			lsIndex.add(i);
 		}
 		
-		showList(lsIndex);
-		showList(lsInTrace);
-		showList(lsOutTrace);
-		RandomizeBySeed(lsIndex, 1);
-		showList(lsIndex);
+//		showList(lsIndex); // all crashes 
+//		showList(lsInTrace); // crashes in trace
+//		showList(lsOutTrace); // crashes out trace
+		
+		int numInTrace = lsInTrace.size();
+		int numOutTrace = lsOutTrace.size();
+		int numInTraceShouldBe = (int) (selNum * (numInTrace *1.0/(numOutTrace + numInTrace)));
+		int numOutTraceShouldBe = selNum - numInTraceShouldBe;
+		
+//		System.out.println("[numInTraceShouldBe ]" + numInTraceShouldBe);
+//		System.out.println("[numOutTraceShouldBe]" + numOutTraceShouldBe);
+		
+		RandomizeBySeed(lsIndex, 1); // randomize all crashes
+//		showList(lsIndex);
+		
+		List<Integer> lsInTraceAfterMize = new ArrayList<Integer>(); // crashes in trace after randomize
+		List<Integer> lsOutTraceAfterMize = new ArrayList<Integer>(); // crashes in trace after randomize
+		
+		for(Integer crash: lsIndex){
+			if(lsInTrace.contains(crash)){
+				lsInTraceAfterMize.add(crash);
+			}else{
+				lsOutTraceAfterMize.add(crash);
+			}
+		}
+		
+//		showList(lsInTraceAfterMize);
+//		showList(lsOutTraceAfterMize);
+		
+		List<Integer> lsFinalCrashes = new ArrayList<Integer>();
+		
+		for(int i=0; i<numInTraceShouldBe; i++){
+			lsFinalCrashes.add(lsInTraceAfterMize.get(i));
+		}
+		for(int j=0;j<numOutTraceShouldBe; j++){
+			lsFinalCrashes.add(lsOutTraceAfterMize.get(j));
+		}
+		
+//		showList(lsFinalCrashes);
+		
+		return lsFinalCrashes;
 	}
 	
-	/** to print the integer list */
+	/** to print the integer list <b>ls</b> */
 	public static void showList(List<Integer> ls){
 		
 		for(Integer in: ls){
@@ -48,13 +105,15 @@ public class RandomSimulator {
 		System.out.println("\n----------------------------------------------------");
 	}
 	
+	/** to randomize the list <b>lsOrigin</b> with random seed <b>seed</b> */
 	public static void RandomizeBySeed(List<Integer> lsOrigin, int seed){
 //	public static List<Integer> RandomizeBySeed(List<Integer> lsOrigin, int seed){
 //		List<Integer> lsRandom = new ArrayList<Integer>();
 		int len = lsOrigin.size();
+		Random rand = new Random(seed);
 		for(int j = len-1; j>0; j--){
-			int changeIndex = (new Random(seed)).nextInt(j+1);
-			System.out.println(" change with : " + changeIndex);
+			int changeIndex = rand.nextInt(j+1);
+//			System.out.printf(" change [%d] with [%d] \n", j, changeIndex);
 			
 			int temp = lsOrigin.get(j);
 			lsOrigin.set(j, lsOrigin.get(changeIndex));
